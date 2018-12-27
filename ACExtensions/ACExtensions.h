247ec28965cf534c4ab8d50bd0bf6af69fb23de9
Adam Cobb <cobb.2326@gmail.com>
2018-12-27 01:28:38 -0800

    Initial Commit

---
+++ ACExtensions/ACExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ACExtensions.h
+//  ACExtensions
+//
+//  Created by Adam Cobb on 12/27/18.
+//  Copyright © 2018 Adam Cobb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACExtensions.
+FOUNDATION_EXPORT double ACExtensionsVersionNumber;
+
+//! Project version string for ACExtensions.
+FOUNDATION_EXPORT const unsigned char ACExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACExtensions/PublicHeader.h>
+
+
