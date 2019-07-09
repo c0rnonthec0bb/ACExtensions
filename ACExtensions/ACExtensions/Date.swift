4c7f9f4d82e359a83c62287c9f6768a8d6880dc8
Adam Cobb <cobb.2326@gmail.com>
2019-07-09 14:32:02 -0400

    Update to version 0.1.19

--- ACExtensions/ACExtensions/Date.swift
+++ ACExtensions/ACExtensions/Date.swift
@@ -18,4 +18,69 @@ public extension Date{
     init(milliseconds: Milliseconds) {
         self.init(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
     }
+    
+    enum TimeMeasurement:Int{
+        case seconds = 1
+        case minutes = 2
+        case hours = 3
+        case days = 4
+        case weeks = 5
+        case years = 6
+        
+        func unitString(shorthand:Bool)->String{
+            switch self{
+            case .seconds:
+                return shorthand ? "sec" : "second"
+            case .minutes:
+                return shorthand ? "min" : "minute"
+            case .hours:
+                return shorthand ? "hr" : "hour"
+            case .days:
+                return shorthand ? "day" : "day"
+            case .weeks:
+                return shorthand ? "wk" : "week"
+            case .years:
+                return shorthand ? "yr" : "year"
+            }
+        }
+        
+        static public func > (left: TimeMeasurement, right: TimeMeasurement) -> Bool {
+            return left.rawValue > right.rawValue
+        }
+        
+        static public func < (left: TimeMeasurement, right: TimeMeasurement) -> Bool {
+            return left.rawValue < right.rawValue
+        }
+    }
+    
+    func agoString(timeIncrements:[TimeMeasurement] = [.seconds, .minutes, .hours, .days, .weeks, .years], useShorthand:Bool = false)->String{
+        
+        // largest increment first (years before seconds)
+        let timeIncrements = timeIncrements.sorted{ a, b in
+            return a > b
+        }
+        
+        var measurementMap:[TimeMeasurement:Int] = [:]
+        
+        measurementMap[.seconds] = Int(Date().timeIntervalSince(self))
+        measurementMap[.minutes] = measurementMap[.seconds]! / 60
+        measurementMap[.hours] = measurementMap[.minutes]! / 60
+        measurementMap[.days] = measurementMap[.hours]! / 24
+        measurementMap[.weeks] = measurementMap[.days]! / 7
+        measurementMap[.years] = Int(Double(measurementMap[.days]!) / 365.2422)
+        
+        for increment in timeIncrements{
+            if let value = measurementMap[increment], value > 0{
+                return "\(value) \(increment.unitString(shorthand: useShorthand))\(value == 1 ? "" : "s") ago"
+            }
+        }
+        
+        return "Now"
+    }
+}
+
+public extension TimeInterval{
+    var milliseconds:Milliseconds{
+        return Milliseconds(self * 1000)
+    }
 }
